5412146bda5a9cb04711271c9a6bdff67d2f4c6e
bbangwon <bbangwon.chung@gmail.com>
2020-01-10 17:55:25 +0900

    UnityEditor State Generator 툴 추가

---
+++ BW.StateMachine.Unity.Editor/StateCodeGenerator.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.CSharp;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BW.StateMachine.Unity.Editor
+{
+    public class StateCodeGenerator
+    {
+        public static void GenerateCSharpCode(CodeCompileUnit unit, string fileName)
+        {
+            string directory = Path.GetDirectoryName(fileName);
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            using (StreamWriter sw = new StreamWriter(fileName, false))
+            {
+                IndentedTextWriter tw = new IndentedTextWriter(sw);
+                provider.GenerateCodeFromCompileUnit(unit, tw, new CodeGeneratorOptions() { BracingStyle = "C", BlankLinesBetweenMembers = false });
+                tw.Close();
+            }
+        }
+
+        public static void Generate(string path, string stateMachineName, List<string> stateNames)
+        {
+            string fileFolderName = Path.Combine(path, "BW.StateMachine", stateMachineName);
+            //1. Enum file Generate
+            CodeCompileUnit machineUnit = new CodeCompileUnit();
+            CodeNamespace codeNamespace = new CodeNamespace();
+
+            string filePath = Path.Combine(fileFolderName, stateMachineName + ".cs");
+            CodeTypeDeclaration machineCode = new CodeTypeDeclaration(stateMachineName);
+            machineCode.IsEnum = true;
+            machineCode.TypeAttributes = System.Reflection.TypeAttributes.Public;
+
+            foreach (var item in stateNames)
+                machineCode.Members.Add(new CodeMemberField(stateMachineName, item));
+
+            codeNamespace.Types.Add(machineCode);
+            machineUnit.Namespaces.Add(codeNamespace);
+
+            GenerateCSharpCode(machineUnit, filePath);
+
+            //2. state Class Generate
+            foreach (var item in stateNames)
+            {
+                CodeCompileUnit stateUnit = new CodeCompileUnit();
+                CodeNamespace stateCodeNamespace = new CodeNamespace();
+                stateCodeNamespace.Imports.Add(new CodeNamespaceImport("BW.StateMachine.Unity"));
+
+                string stateFilePath = Path.Combine(fileFolderName, item + ".cs");
+
+                CodeTypeDeclaration stateCode = new CodeTypeDeclaration(item);
+                stateCode.IsClass = true;
+                stateCode.BaseTypes.Add("StateBase<" + stateMachineName + ">");
+                stateCode.TypeAttributes = System.Reflection.TypeAttributes.Public;
+
+                CodeMemberMethod enterMethod = new CodeMemberMethod();
+                enterMethod.Attributes = MemberAttributes.Public | MemberAttributes.Override;
+                enterMethod.Name = "Enter";
+                enterMethod.ReturnType = new CodeTypeReference(typeof(void));
+                stateCode.Members.Add(enterMethod);
+
+                CodeMemberMethod updateMethod = new CodeMemberMethod();
+                updateMethod.Attributes = MemberAttributes.Public | MemberAttributes.Override;
+                updateMethod.Name = "Update";
+                updateMethod.ReturnType = new CodeTypeReference(typeof(void));
+                stateCode.Members.Add(updateMethod);
+
+                CodeMemberMethod exitMethod = new CodeMemberMethod();
+                exitMethod.Attributes = MemberAttributes.Public | MemberAttributes.Override;
+                exitMethod.Name = "Exit";
+                exitMethod.ReturnType = new CodeTypeReference(typeof(void));
+                stateCode.Members.Add(exitMethod);
+
+                stateCodeNamespace.Types.Add(stateCode);
+                stateUnit.Namespaces.Add(stateCodeNamespace);
+
+                GenerateCSharpCode(stateUnit, stateFilePath);
+            }
+        }
+    }
+}
---
+++ BW.StateMachine.Unity.Editor/StateCodeGeneratorWindow.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BW.StateMachine.Unity.Editor
+{
+    class StateCodeGeneratorWindow : EditorWindow
+    {
+        private static GUIContent generatorButton = new GUIContent("Generate", "Generate StateMachine");
+
+        string statesName = "MyState";  //DefaultStateName = MyState
+
+        ReorderableList list = null;
+
+        List<string> stateList = new List<string>();
+
+        [MenuItem("BW.StateMachine/StateMachine Generator")]
+        public static void ShowWindow()
+        {
+            GetWindow<StateCodeGeneratorWindow>(true, "StateMachine Generator");
+        }
+
+        void OnEnable()
+        {
+            list = new ReorderableList(stateList, typeof(List<string>));
+
+            list.onAddCallback = (ReorderableList list) => {
+                list.list.Add("State_" + list.list.Count);
+            };
+
+            list.drawHeaderCallback = (Rect rect) =>
+            {
+                EditorGUI.LabelField(rect, "State Names");
+            };
+
+            list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+            {
+                var element = (string)list.list[index];
+                //rect.y += 2;
+                list.list[index] = EditorGUI.TextField(rect, element);
+            };
+        }
+
+        void OnGUI()
+        {
+            statesName = EditorGUILayout.TextField("StateMachine Name", statesName);
+            list.DoLayoutList();
+
+
+            EditorGUI.BeginDisabledGroup(stateList.Count == 0);
+
+            if (GUILayout.Button(generatorButton))
+            {
+                if (stateList.Count >= 2)
+                {
+                    if (stateList.Distinct().Count() != stateList.Count)
+                    {
+                        EditorUtility.DisplayDialog("Error!", "State name must be unique.", "OK");
+                        return;
+                    }
+                }
+
+                //Regex
+                Regex regex = new Regex(@"^[A-Za-z_]\w*\w$");
+                if (!regex.IsMatch(statesName))
+                {
+                    EditorUtility.DisplayDialog("Error!", $"It can't set StateMachine '{statesName}'", "OK");
+                    return;
+                }
+
+                foreach (var item in stateList)
+                {
+                    if (!regex.IsMatch(item))
+                    {
+                        EditorUtility.DisplayDialog("Error!", $"It can't set State '{item}'", "OK");
+                        return;
+                    }
+                }
+                //~Regex
+
+                if (EditorUtility.DisplayDialog("Confirm", $"Do you want '{statesName}' generate?", "Yes", "No"))
+                {
+                    StateCodeGenerator.Generate(Application.dataPath, statesName, stateList);
+                    AssetDatabase.Refresh();
+                }
+            }
+
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}
